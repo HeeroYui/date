a678f05638e3f6e6572286721b2db8d92b5fb528
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_date.py
+++ lutin_date.py
@@ -3,26 +3,36 @@ import lutin.module as module
 import lutin.tools as tools
 import datetime
 
+def get_type():
+	return "LIBRARY"
+
 def get_desc():
 	return "Date buid date of the program"
 
+def get_licence():
+	return "APACHE-2"
 
-def create(target):
-	# module name is 'edn' and type binary.
-	my_module = module.Module(__file__, 'date', 'LIBRARY')
-	# add the file to compile:
-	
-	
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def get_version():
+	return [0,1]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_src_file([
 		'date/date.cpp'
 		])
-	
 	my_module.add_header_file([
 		'date/date.h'
 		])
-	
 	now = datetime.datetime.now()
-	
 	my_module.compile_flags('c++', [
 		'-Wno-write-strings',
 		'-Wall',
@@ -32,10 +42,7 @@ def create(target):
 		"-DBUILD_HOUR=\""+str(now.hour)+"\"",
 		"-DBUILD_MINUTE=\""+str(now.minute)+"\"",
 		"-DBUILD_SECOND=\""+str(now.second)+"\""])
-	
 	my_module.add_path(tools.get_current_path(__file__))
-	
-	# add the currrent module at the 
 	return my_module
 
 
